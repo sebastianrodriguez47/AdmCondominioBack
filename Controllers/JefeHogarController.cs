e9722934eb72116bba19bba76e89b63dea9ffce9
sebastianrodriguez47 <69803933+sebastianrodriguez47@users.noreply.github.com>
2024-04-26 16:53:44 -0400

    Add files via upload

---
+++ Controllers/JefeHogarController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdmCondominioBack.Controllers
+{
+    [ApiController]
+    [Route("jefeHogar")]
+    public class JefeHogarController : ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+        public dynamic listarJefesHogar()
+        {
+
+        }
+        [HttpGet]
+        [Route("listar/{id}")]
+        public dynamic listarJefesHogarById()
+        {
+
+        }
+
+
+    }
+}
