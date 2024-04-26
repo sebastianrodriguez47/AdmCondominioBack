603a45f4641ab87c31cc878b13d7a0b1d91deea2
Sebastián Rodríguez <sebastian.rodriguez@finix-group.com>
2024-04-26 15:35:12 -0400

    Se Añade El Proyecto

---
+++ Controllers/CasaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdmCondominioBack.Controllers
+{
+    [ApiController]
+    [Route("casa")]
+    public class CasaController : ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+        public Task<IActionResult> listarCasas(string search, string currentOrder, int? pagNum, string currentFilter)
+        {
+
+            return;
+        }
+        [HttpGet]
+        [Route("listar/{id}")]
+        public Task<IActionResult> listarCasaById()
+        {
+            return;
+        }
+
+
+    }
+}
